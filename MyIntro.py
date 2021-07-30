f7e20b15e62f27d041125f64f9f4ec1aea119a54
koshishtiwari <88152625+koshishtiwari@users.noreply.github.com>
2021-07-29 20:22:17 -0400

    MyIntro

---
+++ MyIntro.py
@@ -0,0 +1,6 @@
+a = input ('enter what you want to know about me (name, age, etc) : ')
+if (a == 'name') : print ('Koshish Tiwari') 
+elif (a == 'age') : print ('18')
+elif (a == 'class') : print ('Bachlors')
+elif (a == 'school') : print ('Caldwell University')
+else : print ('wrong entry')
