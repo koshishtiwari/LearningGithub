7038227d33ee14c3c6259d00cbffbbd98d0bd0cc
Gaurav Tiwari <88152625+koshishtiwari@users.noreply.github.com>
2021-07-31 13:14:01 -0400

    Update Guess.py

--- Guess.py
+++ Guess.py
@@ -1,16 +1,9 @@
-import random
-
-def guess(x):
-    random_number = random.randint(1,x)
-    guess = 0
-    while guess != random_number:
-        guess = int(input (f'guess a number between 1 and {x} : '))
-        if guess < random_number:
-            print ('enter higher values ')
-
-        elif guess> random_number:
-            print ('enter lower values ')
-        
-    print (f'Congrats! your number is {random_number}')
-
-guess(10)
+from random import randrange
+random_number = randrange(10)
+while True:
+    guess = int(input('guess a number : '))
+    if guess == random_number:
+        print ('You Won! ')
+        break
+    print ('guess higher values' if ( guess < random_number) else 'guess lower values')
+    
